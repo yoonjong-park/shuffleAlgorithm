2f5db7dae3d58fcf63a68d6ea396cd0a996aa055
yoonjong.park <68855211+yoonjong-park@users.noreply.github.com>
2020-09-05 11:13:07 +0900

    NEW

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let lottoNubmers = Array(45);
+lottoNubmers.fill();
+console.log(lottoNubmers);
+
+for(i=0; i=lottoNubmers.length; i++){
+    lottoNubmers[i]=i+1;
+}
+
+
+
+// let number = lottoNubmers.map(function(a,number){
+//     return number+1;
+// });
+console.log(lottoNubmers);
+console.log(number);
+
+
+
